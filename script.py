8986d01ed73ce21ffb0adb42486456ce24cbde0a
azvno <yeelenbaaboura@gmail.com>
2024-10-18 14:14:29 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,11 @@
-import os
+import requests
+
+script_url = 'https://raw.githubusercontent.com/azvno/newtab/main/script.py'
+
+def update_script():
+    response = requests.get(script_url)
+    if response.status_code == 200:
+        with open('script.py', 'wb') as f:
+            f.write(response.content)
+
+update_script()
